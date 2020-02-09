3b48f8b11e9d5a6e3ef5817119bab9aeeee6d53d
wessel mast <mastwessel@gmail.com>
2020-02-10 00:38:00 +0100

    Init

---
+++ NodeEditor/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Media.Effects;
+
+namespace WpfApp1 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private List<Node> nodes = new List<Node>();
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void spawnNode_Click(object sender, RoutedEventArgs e) {
+            DrawNode(new Size(100, 50), (Color)ColorConverter.ConvertFromString("#FFf39204"), Mouse.GetPosition(canvas));
+        }
+
+        private void DrawNode(Size size, Color color, Point position) {
+            Node node = new Node(size, color, position);
+
+            BindNode(node);
+            nodes.Add(node);
+            node.Add(ref canvas);
+
+            node.Render();
+        }
+
+        private void Node_OnClicked(Node node, MouseButtonEventArgs e) {
+            foreach(Node n in nodes) {
+                if (n == node) continue;
+                n.SetZIndex(n.GetZIndex() - 1);
+            }
+            node.SetZIndex(nodes.Count);
+        }
+
+        private void Node_OnMoved(Node node, MouseEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                node.SetPosition(e.GetPosition(canvas));
+            }
+        }
+
+        private void Node_OnDelete(Node node) {
+            UnbindNode(node);
+            nodes.Remove(node);
+            node.Remove(ref canvas);
+        }
+
+        private void BindNode(Node node) {
+            node.OnMoved += Node_OnMoved;
+            node.OnClicked += Node_OnClicked;
+            node.OnDelete += Node_OnDelete;
+        }
+
+        private void UnbindNode(Node node) {
+            node.OnMoved -= Node_OnMoved;
+            node.OnClicked -= Node_OnClicked;
+            node.OnDelete -= Node_OnDelete;
+        }
+    }
+}
---
+++ NodeEditor/Node.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Media.Effects;
+
+namespace WpfApp1 {
+    public class Node {
+        public Size size;
+        public Color color;
+        public Point position;
+
+        public event Action<Node> OnDelete = delegate { };
+        public event Action<Node, MouseEventArgs> OnMoved = delegate { };
+        public event Action<Node, MouseButtonEventArgs> OnClicked = delegate { };
+
+        private Rectangle rect = new Rectangle();
+
+        public Node(Size size, Color color, Point position) {
+            this.size = size;
+            this.color = color;
+            this.position = position;
+
+            BindEvents();
+            AddContextMenu();
+        }
+
+        public void Add(ref Canvas canvas) {
+            canvas.Children.Add(GetRect());
+        }
+
+        public void Remove(ref Canvas canvas) {
+            canvas.Children.Remove(GetRect());
+        }
+
+        public Rectangle GetRect() {
+            return rect;
+        }
+
+        private void BindEvents() {
+            rect.MouseMove += (object sender, MouseEventArgs e) => { OnMoved(this, e); };
+            rect.MouseDown += (object sender, MouseButtonEventArgs e) => { OnClicked(this, e); };
+        }
+
+        private void AddContextMenu() {
+            MenuItem mi = new MenuItem();
+            mi.Header = "Delete";
+            mi.Name = "Mi";
+            mi.Click += Mi_Click;
+            mi.DataContext = rect;
+            ContextMenu contextMenu = new ContextMenu();
+            contextMenu.Items.Add(mi);
+            rect.ContextMenu = contextMenu;
+        }
+
+        public void Render() {
+            DropShadowEffect effect = new DropShadowEffect();
+            effect.BlurRadius = 10;
+            effect.ShadowDepth = 2;
+            rect.Effect = effect;
+            rect.Stroke = new SolidColorBrush(color);
+            rect.Fill = new SolidColorBrush(color);
+            rect.Width = size.Width;
+            rect.Height = size.Height;
+            SetPosition(position);
+        }
+
+        public void SetPosition(Point position) {
+            Canvas.SetLeft(rect, position.X - rect.Width * 0.5f);
+            Canvas.SetTop(rect, position.Y - rect.Height * 0.5f);
+        }
+
+        public void SetZIndex(int index) {
+            Canvas.SetZIndex(rect, index);
+        }
+
+        public int GetZIndex() {
+            return Canvas.GetZIndex(rect);
+        }
+
+        private void Mi_Click(object sender, RoutedEventArgs e) {
+            MenuItem mi = sender as MenuItem;
+            if (mi == null) return;
+            Rectangle rect = mi.DataContext as Rectangle;
+            if (rect == null) return;
+            OnDelete(this);
+        }
+    }
+}
